9ff6da94c2005bc9d079fcd7c3a0bce6724520bd
LucaFin <luca.finoia@gmail.com>
2021-08-26 15:52:06 +0200

    Creating the Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Image playerImage;
+    public Image mirrorImage;
+    public Image healthNumberImage;
+    public Image glowImage;
+
+    public int health = 5;
+    public int mana = 1;
+
+    public bool isPlayer;
+    public bool isFire;
+
+    public GameObject[] manaBalls = new GameObject[5];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
